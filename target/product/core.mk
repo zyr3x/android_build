dbbe2965365cf8863715db857cd10a4e7e683403
Liukai <liukai@shendu.com>
2012-10-18 16:13:09 +0800

    o fix usb
    add this , can view logcat during bootanimation

--- target/product/core.mk
+++ target/product/core.mk
@@ -121,7 +121,8 @@ PRODUCT_PACKAGES := \
 
 PRODUCT_COPY_FILES += \
     system/core/rootdir/init.trace.rc:root/init.trace.rc \
-#    system/core/rootdir/init.usb.rc:root/init.usb.rc \
+    system/core/rootdir/init.usb.rc:root/init.usb.rc
+
 # host-only dependencies
 ifeq ($(WITH_HOST_DALVIK),true)
     PRODUCT_PACKAGES += \
